79de758722d6867ebcd0aac9fac925a49ff2c2de
Clayton Gray <claytonjgray@gmail.com>
2019-06-17 12:11:53 -0400

    Initial Commit

---
+++ screenshoot-chromext/src/inject/inject.js
@@ -0,0 +1,151 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// $('body').append('<div style="position:fixed; top:0; right:0;"><a href="'+secretLink+'">TEST</a></div>');
+
+		var div = document.createElement( 'div' );
+
+		//append all elements
+		document.body.appendChild( div );
+
+		//set attributes for div
+		div.id = 'screenshoot_container';
+		div.innerHTML = "<strong>Screenshot:</strong>";
+
+		// Screenshot Button - 375
+		var button375 = document.createElement( 'button' );
+		button375.id="screenshot_button_375";
+		button375.innerHTML = "375";
+		div.appendChild( button375 );
+
+		// Screenshot Button - 1024
+		var button1024 = document.createElement( 'button' );
+		button1024.id="screenshot_button_1024";
+		button1024.innerHTML = "1024";
+		div.appendChild( button1024 );
+
+		// Screenshot Button - 1440
+		var button1440 = document.createElement( 'button' );
+		button1440.id="screenshot_button_1440";
+		button1440.innerHTML = "1440";
+		div.appendChild( button1440 );
+
+		// Screenshot Button - All
+		var buttonAll = document.createElement( 'button' );
+		buttonAll.id="screenshot_button_all";
+		buttonAll.innerHTML = "All";
+		div.appendChild( buttonAll );
+
+		// options
+		var optDiv = document.createElement('div');
+		optDiv.id = "screenshot_options";
+		div.appendChild(optDiv);
+
+		// full page checkbox
+		var fplabel = document.createElement('label');
+		fplabel.id = "fullpage-label";
+
+		// add the fullpage checkbox
+		fplabel.innerHTML = '<input type="checkbox" value="true" id="fullpage_checkbox" /> Full Page';
+		optDiv.appendChild(fplabel);
+
+		// update checkbox state
+		chrome.storage.sync.get('fullpage', function(data) {
+			console.log("get checked status", data, data.fullpage);
+		    document.getElementById('fullpage_checkbox').checked = data.fullpage;
+		});
+
+		var fpcb = document.getElementById('fullpage_checkbox');
+
+		fpcb.addEventListener('change', function (e) {
+			console.log("change");
+			console.log(e.target.checked);
+			chrome.storage.sync.set({ fullpage: e.target.checked });
+		});
+
+		// var fpcb = document.createElement('input');
+		// fpcb.id = "fullpage_checkbox";
+		// fpcb.type = "checkbox";
+		// fpcb.value = "true";
+		// fpcb.checked = true;
+		// fplabel.appendChild(fpcb);
+
+		function buttonClick(e) {
+			// get filename
+			// get secret link
+
+			// console.log(e);
+			// console.log(e.target);
+			// console.log(e.currentTarget);
+
+			if (document.getElementById("sitemapLinkWithPlayer")) {
+				var secretLink =  document.getElementById("sitemapLinkWithPlayer").value;
+
+				// console.log("secretLink", secretLink);
+
+				// get filename
+				var filename;
+				if (document.getElementsByClassName('sitemapHighlight').length > 0) {
+					filename = document.getElementsByClassName('sitemapHighlight')[0].getElementsByClassName('sitemapPageName')[0].innerHTML;
+				}
+
+				var href = "http://localhost:3000/?page="+secretLink;
+
+				if (e.target.id == "screenshot_button_375") {
+					href = href+"&viewport_mobile=true";
+				}
+
+				if (e.target.id == "screenshot_button_1024") {
+					href = href+"&viewport_mobile=false&viewport_desktop=true";
+				}
+
+				if (e.target.id == "screenshot_button_1440") {
+					href = href+"&viewport_mobile=false&viewport_desktop_big=true";
+				}
+
+				if (e.target.id == "screenshot_button_all") {
+					href = href+"&viewport_mobile=true&viewport_desktop=true&viewport_desktop_big=true";
+				}
+
+				var fullpage = document.getElementById('fullpage_checkbox');
+				if (!fullpage.checked) {
+					href = href+"&fullpage=false";
+				}
+
+				if (filename) {
+					href = href+"&filename="+filename;
+				}
+
+				console.log("href", href);
+
+
+				// create link, click it, and remove it.
+				var a = document.createElement( 'a' );
+				div.appendChild( a );
+				a.id ="gotoScreenshot";
+				a.href = href;
+				a.target = "_blank";
+				document.getElementById('gotoScreenshot').click();
+				a.remove();
+
+			} else {
+				alert("Could not find secret link. Please try again.");
+			}
+		}
+
+		document.getElementById('screenshot_button_375').addEventListener('click', buttonClick);
+		document.getElementById('screenshot_button_1024').addEventListener('click', buttonClick);
+		document.getElementById('screenshot_button_1440').addEventListener('click', buttonClick);
+
+		document.getElementById('screenshot_button_all').addEventListener('click', buttonClick);
+
+
+
+
+
+
+	}
+	}, 10);
+});
---
+++ screenshoot-service/app.js
@@ -0,0 +1,155 @@
+var express = require("express");
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+
+let viewport_sizing = {
+    "mobile": { width: 375, height: 667 },
+    "desktop": { width: 1024, height: 768 },
+    "desktop_big": { width: 1440, height: 768 }
+};
+
+
+async function screenshot_a_link(secretLink, filename, res, viewports, fullPage) {
+  console.log("screenshot_a_link");
+  console.log(secretLink);
+  var timerStart = Date.now();
+
+
+    let browser = await puppeteer.launch({ headless: true });
+   
+    for (let i = 0; i < viewports.length; i++) {
+        var page2 = await browser.newPage();
+
+        // "mobile", "desktop", "desktop_big"
+        var viewportName = viewports[i];
+
+        // set the viewport based on which viewport we're doing now.
+        await page2.setViewport( viewport_sizing[ viewportName ] );
+
+        // we can emulate phone, but has retina screens which makes things blurry.
+        // await page2.emulate(iPhone8);
+
+        try {
+
+            await page2.goto(secretLink)
+
+            await page2.waitForSelector('body');
+            await page2.waitFor(500);
+
+            var folder;
+            
+            if (viewportName == "mobile") {
+                folder = "mobile/";
+            }
+            
+            if (viewportName == "desktop") {
+                folder = "desktop/";
+            }
+            
+            if (viewportName == "desktop_big") {
+                folder = "desktop_big/";
+            }
+
+            // await page2.waitForNavigation({ waitUntil: 'load' });
+            await page2.screenshot({ path: 'public/screenshots/'+folder+filename+'.png', fullPage: fullPage });
+        }
+
+
+        catch (error) {
+
+            let generationTime = await (Date.now()-timerStart) / 1000;
+
+            res.render("error", {error: error, page: secretLink, filename: filename+'.png', time: generationTime, fullPage: fullPage});
+        }
+
+        // // create better image title based on title
+
+        // await page.screenshot({ path: './image2.png', fullPage: true });
+        await page2.close();
+    }
+
+    
+    await browser.close();
+
+    // await res.json({"success":"success", "page": secretLink, "filename": filename+'.png'});
+
+    let generationTime = await (Date.now()-timerStart) / 1000;
+
+    await res.render("success", {page: secretLink, filename: filename+'.png', time: generationTime, totalShots: viewports.length, viewports: viewports, fullPage: fullPage});
+}
+
+var toTitleCase = function (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+};
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+app.get("/", (req, res, next) => {
+
+	// var response = {'response':'nothing', page: req.query.page};
+	var page = req.query.page;
+    var mobile = req.query.viewport_mobile || true;
+    var desktop = req.query.viewport_desktop || false;
+    var desktop_big = req.query.viewport_desktop_big || false;
+
+    // fullpage default
+    var fullPage = true;
+
+    if (req.query.fullpage == "false") {
+        fullPage = false;
+    }
+
+    var filename;
+
+    // var viewports = {
+    //     mobile: mobile,
+    //     desktop: desktop,
+    //     desktop_big: desktop_big
+    // }
+
+    // gather the viewports we're doing. (most likely just 1, mobile)
+    var viewports = [];
+    if (mobile != "false") { viewports.push('mobile'); }
+    if (desktop) { viewports.push('desktop'); }
+    if (desktop_big) { viewports.push('desktop_big'); }
+
+        console.log(viewports);
+
+
+    if (req.query.filename) {
+        filename = decodeURIComponent((req.query.filename + '').replace(/\+/g, '%20'));
+    } else {
+        // try to make a better looking filename
+		filename = page.match(/([^\/]+)(?=\.\w+$)/gm).toString();
+        filename = replaceAll(filename, "_", " ");
+        filename = toTitleCase(filename);
+    }
+	
+    // res.json({mobile: mobile, desktop: desktop, desktop_big: desktop_big, page: page, filename: filename+'.png'});
+
+
+	screenshot_a_link(page, filename, res, viewports, fullPage);
+
+    // res.send("test");
+
+});
+
+// app.get("/:page", (req, res, next) => {
+//  res.json({
+//      page: req.params['page']
+//  }); 
+
+// });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
